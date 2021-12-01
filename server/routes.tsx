32f599cdf44e648fb8c2d6ea0f38cff04ff6e32a
Jason Cheatham <jason@jasoncheatham.com>
2021-11-30 22:55:05 -0500

    Use Google font

--- server/routes.tsx
+++ server/routes.tsx
@@ -16,6 +16,9 @@ export function createRouter(bundle: { path: string; text: string }) {
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-touch-fullscreen" content="yes">
+        <link rel="preconnect" href="https://fonts.googleapis.com"> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="/styles.css">
         ${
       user
