49153e43899bd4cc0c79f221f74f045e59652b62
Jason Cheatham <jason@jasoncheatham.com>
2021-12-09 09:29:42 -0500

    Create user_articles records when necessary

--- server/database/user_articles.ts
+++ server/database/user_articles.ts
@@ -1,4 +1,5 @@
 import { getDb, query } from "./db.ts";
+import { log } from "../deps.ts";
 
 export interface UserArticle {
   id: number;
@@ -27,36 +28,37 @@ export function setArticlesRead(
   userId: number,
   patches: { articleId: number; read: boolean }[],
 ): void {
-  query('BEGIN TRANSACTION');
+  query("BEGIN TRANSACTION");
 
   const statement = getDb().prepareQuery(
-    `UPDATE user_articles
-    SET read = (:read)
-    WHERE user_id = (:userId) AND article_id = (:articleId)`,
+    `INSERT INTO user_articles (user_id, article_id, read)
+    VALUES (:userId, :articleId, :read)
+    ON CONFLICT(user_id, article_id) DO UPDATE SET read = (:read)`
   );
 
   for (const patch of patches) {
     statement.execute({ userId, ...patch });
+    log.debug(`Set article ${patch.articleId}.read to ${patch.read}`);
   }
 
-  query('END TRANSACTION');
+  query("END TRANSACTION");
 }
 
 export function setArticlesSaved(
   userId: number,
   patches: { articleId: number; saved: boolean }[],
 ) {
-  query('BEGIN TRANSACTION');
+  query("BEGIN TRANSACTION");
 
   const statement = getDb().prepareQuery(
-    `UPDATE user_articles
-    SET saved = (:saved)
-    WHERE user_id = (:userId) AND article_id = (:articleId)`,
+    `INSERT INTO user_articles (user_id, article_id, saved)
+    VALUES (:userId, :articleId, :saved)
+    ON CONFLICT(user_id, article_id) DO UPDATE SET saved = (:saved)`
   );
 
   for (const patch of patches) {
     statement.execute({ userId, ...patch });
   }
 
-  query('END TRANSACTION');
+  query("END TRANSACTION");
 }
