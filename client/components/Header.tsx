7f859d8e8a0a56874d013db4c4924c05ec40c979
Jason Cheatham <jason@jasoncheatham.com>
2021-12-12 16:08:12 -0500

    Header updates
    - Show selected feed title in header
    - Remove header buttons
    - Add selected feed title to FeedsContext

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,7 +1,5 @@
-import { React, useCallback, useState } from "../deps.ts";
-import { refreshFeeds, reprocess } from "../api.ts";
-import { useUser } from "../contexts/mod.tsx";
-import Button from "./Button.tsx";
+import { React } from "../deps.ts";
+import { useUser, useFeeds } from "../contexts/mod.tsx";
 
 export interface HeaderProps {
   onShowSidebar?: () => void;
@@ -10,43 +8,18 @@ export interface HeaderProps {
 const Header: React.FC<HeaderProps> = (props) => {
   const { onShowSidebar } = props;
   const { user } = useUser();
-  const [busy, setBusy] = useState(false);
-
-  const update = useCallback(async () => {
-    setBusy(true);
-    try {
-      await refreshFeeds();
-    } catch (error) {
-      console.warn(error);
-    } finally {
-      setBusy(false);
-    }
-  }, []);
-
-  const repro = useCallback(async () => {
-    setBusy(true);
-    try {
-      await reprocess();
-    } catch (error) {
-      console.warn(error);
-    } finally {
-      setBusy(false);
-    }
-  }, []);
+  const { feedsTitle } = useFeeds();
 
   return (
     <header className="Header">
-      <h1 onClick={onShowSidebar}>Simple News</h1>
-      <div className="Header-buttons">
-        <Button label="Refresh" onClick={update} disabled={busy} size="small" />
-        <Button
-          label="Reprocess"
-          onClick={repro}
-          disabled={busy}
-          size="small"
-        />
+      <div className="Header-left" onClick={onShowSidebar}>
+        <img src="favicon-192x192.png"/>
+        <h1>Simple News</h1>
       </div>
-      <div className="Header-user">{user?.name}</div>
+      <div className="Header-center">
+        <h2>{feedsTitle}</h2>
+      </div>
+      <div className="Header-right">{user?.name}</div>
     </header>
   );
 };
--- client/contexts/FeedsContext.tsx
+++ client/contexts/FeedsContext.tsx
@@ -1,8 +1,18 @@
 import { React, useContext, useMemo } from "../deps.ts";
+import { User } from "../../types.ts";
+import { useUser } from "./UserContext.tsx";
 
 const FeedsContext = React.createContext<
-  { selectedFeeds: number[]; setSelectedFeeds: (feedIds: number[]) => void }
->({ selectedFeeds: [], setSelectedFeeds: () => undefined });
+  {
+    selectedFeeds: number[];
+    setSelectedFeeds: (feedIds: number[]) => void;
+    feedsTitle: string | undefined;
+  }
+>({
+  selectedFeeds: [],
+  setSelectedFeeds: () => undefined,
+  feedsTitle: undefined,
+});
 
 export default FeedsContext;
 
@@ -10,14 +20,47 @@ export interface FeedsProviderProps {
   selectedFeeds?: number[] | undefined;
 }
 
+function getFeedsTitle(selectedFeeds: number[], user: User | undefined) {
+  if (selectedFeeds.length === 0) {
+    return undefined;
+  }
+  const feedGroups = user?.config?.feedGroups;
+
+  if (!feedGroups) {
+    return undefined;
+  }
+
+  if (selectedFeeds.length === 1) {
+    for (const group of feedGroups) {
+      for (const feed of group.feeds) {
+        if (feed.id === selectedFeeds[0]) {
+          return feed.title;
+        }
+      }
+    }
+  } else if (selectedFeeds.length > 1) {
+    for (const group of feedGroups) {
+      for (const feed of group.feeds) {
+        if (feed.id === selectedFeeds[0]) {
+          return group.title;
+        }
+      }
+    }
+  }
+
+  return undefined;
+}
+
 export const FeedsProvider: React.FC<FeedsProviderProps> = (props) => {
   const [selectedFeeds, setSelectedFeeds] = React.useState<number[]>(
     props.selectedFeeds ?? [],
   );
+  const { user } = useUser();
 
-  const value = useMemo(() => ({ selectedFeeds, setSelectedFeeds }), [
-    selectedFeeds,
-  ]);
+  const value = useMemo(() => {
+    const feedsTitle = getFeedsTitle(selectedFeeds, user);
+    return { selectedFeeds, setSelectedFeeds, feedsTitle };
+  }, [selectedFeeds, user?.config]);
 
   return (
     <FeedsContext.Provider value={value}>
