f29549adc7d4e5f89cf14c8df0eebb7be8e779c0
Jason Cheatham <jason@jasoncheatham.com>
2022-06-18 11:32:06 -0400

    Fix ButtonSelector styles

--- src/lib/components/ButtonSelector.svelte
+++ src/lib/components/ButtonSelector.svelte
@@ -9,13 +9,40 @@
 
 <div class="ButtonSelector">
   {#each options as opt (opt.value)}
-    <Button
-      key={opt.value}
+    <button
       class={selected === opt.value ? 'selected' : ''}
-      {size}
       on:click={() => onSelect(opt.value)}
     >
       {opt.label}
-    </Button>
+    </button>
   {/each}
 </div>
+
+<style>
+  .selected {
+    background: var(--active-matte);
+  }
+
+  button {
+    background: var(--matte);
+    color: var(--foreground);
+    font-size: var(--font-size);
+    border: solid 1px var(--border);
+    border-radius: 0;
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    margin: 0;
+    padding: 0.25em 0.5em;
+    font-size: calc(var(--font-size) * 0.75);
+  }
+
+  button:first-child {
+    border-top-left-radius: var(--border-radius);
+    border-bottom-left-radius: var(--border-radius);
+  }
+  button:last-child {
+    border-top-right-radius: var(--border-radius);
+    border-bottom-right-radius: var(--border-radius);
+  }
+</style>
