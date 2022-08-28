97a1fbbf6022b58a0a52bb0b70cffd82853a41f3
Jason Cheatham <jason@jasoncheatham.com>
2022-08-28 02:16:18 -0400

    Add Meta table to db

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -6,7 +6,16 @@ let db: Database | undefined;
 
 export function getDb() {
   if (!db) {
-    db = sqlite3('data.db');
+    const d = sqlite3('data.db');
+    let version = getVersion(d);
+    if (typeof version !== 'number') {
+      initDb(d);
+      version = 0;
+    }
+
+    // do migrations
+
+    db = d;
   }
   return db;
 }
@@ -19,4 +28,24 @@ export function closeDb() {
 export function transaction(queries: () => void) {
   const db = getDb();
   db.transaction(queries)();
+}
+
+function getVersion(d: Database): number | undefined {
+  try {
+    const ver = d.prepare('SELECT schema_version FROM Meta').get();
+    return ver.schema_version;
+  } catch (error) {
+    return undefined;
+  }
+}
+
+function initDb(d: Database) {
+  db?.transaction(() => {
+    d.exec(
+      `CREATE TABLE IF NOT EXISTS "Meta" (
+      "schema_version" INT NOT NULL
+    )`
+    );
+    d.exec(`INSERT INTO Meta (schema_version) VALUES (0)`);
+  })();
 }
