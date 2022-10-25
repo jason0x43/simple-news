7d5d8069bc7076ab1ea9ffab482e215867fa918f
Jason Cheatham <jason@jasoncheatham.com>
2022-10-24 21:13:14 -0400

    Cleanup
    - Remove unnecessary await
    - Use window instead of globalThis
    - Add a comment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,55 +1,55 @@
 <script type="ts">
-  import '../app.css';
-  import { onMount } from 'svelte';
-  import { createStores } from '$lib/stores';
-  import { setAppContext } from '$lib/contexts';
-  import Toast from '$lib/components/Toast.svelte';
+	import '../app.css';
+	import { onMount } from 'svelte';
+	import { createStores } from '$lib/stores';
+	import { setAppContext } from '$lib/contexts';
+	import Toast from '$lib/components/Toast.svelte';
 
-  let ref: HTMLElement;
-  const stores = createStores();
+	let ref: HTMLElement;
+	const stores = createStores();
 
-  setAppContext({
-    getRoot: () => ref,
-    stores
-  });
+	setAppContext({
+		getRoot: () => ref,
+		stores
+	});
 
-  const { displayType } = stores;
+	const { displayType } = stores;
 
-  onMount(() => {
-    const matcher = globalThis.matchMedia('(max-width: 800px)');
-    const listener = (event: MediaQueryListEvent) => {
-      $displayType = event.matches ? 'mobile' : 'desktop';
-    };
-    matcher.addEventListener('change', listener);
-    $displayType = matcher.matches ? 'mobile' : 'desktop';
+	onMount(() => {
+		const matcher = window.matchMedia('(max-width: 800px)');
+		const listener = (event: MediaQueryListEvent) => {
+			$displayType = event.matches ? 'mobile' : 'desktop';
+		};
+		matcher.addEventListener('change', listener);
+		$displayType = matcher.matches ? 'mobile' : 'desktop';
 
-    return () => {
-      matcher.removeEventListener('change', listener);
-    };
-  });
+		return () => {
+			matcher.removeEventListener('change', listener);
+		};
+	});
 </script>
 
 <div id="root" bind:this={ref}>
-  <slot />
-  <Toast />
+	<slot />
+	<Toast />
 </div>
 
 <style>
-  #root {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    font-family: sans-serif;
-    font-size: var(--font-size);
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    overflow: hidden;
-    color: var(--foreground);
-    position: fixed;
-    width: 100%;
-    background: var(--background);
-  }
+	#root {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		font-family: sans-serif;
+		font-size: var(--font-size);
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		overflow: hidden;
+		color: var(--foreground);
+		position: fixed;
+		width: 100%;
+		background: var(--background);
+	}
 </style>
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,9 @@
 import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
+/**
+ * Redirect users when accessing the root path
+ */
 export const load: PageServerLoad = ({ locals }) => {
 	if (!locals.user) {
 		throw redirect(307, '/login');
