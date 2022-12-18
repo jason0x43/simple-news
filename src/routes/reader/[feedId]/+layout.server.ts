0e998b362aa7ef5030584a47e429e2fbb115c0f0
Jason Cheatham <jason@jasoncheatham.com>
2022-12-18 12:34:23 -0500

    Throw a redirect in the feedId layout

--- src/routes/reader/[feedId]/+layout.server.ts
+++ src/routes/reader/[feedId]/+layout.server.ts
@@ -1,17 +1,16 @@
 import { getArticleHeadings } from '$lib/db/article';
 import { getFeedGroupWithFeeds } from '$lib/db/feedgroup';
 import type { Feed } from '$lib/db/schema';
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals, params }) => {
-	const userId = locals.user?.id;
-	if (!userId) {
-		return {
-			status: 302,
-			redirect: '/login'
-		};
+	if (!locals.user) {
+		console.log('no active user -- redirecting to login');
+		throw redirect(302, '/login');
 	}
 
+	const userId = locals.user.id;
 	const feedId = params.feedId;
 	const [type, id] = feedId.split('-');
 	let feedIds: Feed['id'][];
