a3dbc3d532ef4c4b0108c3aea4c536da2694d215
Jason Cheatham <jason@jasoncheatham.com>
2023-02-19 15:34:40 -0500

    fix: await an async function call

--- src/routes/reader/[feedId]/[articleId]/+page.server.ts
+++ src/routes/reader/[feedId]/[articleId]/+page.server.ts
@@ -2,13 +2,13 @@ import { getArticle } from '$lib/db/article';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = ({ locals, params }) => {
+export const load: PageServerLoad = async ({ locals, params }) => {
 	const user = locals.user;
 	if (!user) {
 		throw error(403, 'not logged in');
 	}
 
-	const article = getArticle({
+	const article = await getArticle({
 		id: params.articleId as string,
 		userId: user.id
 	});
