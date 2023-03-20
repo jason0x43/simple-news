c95dd596dcb9c819d3fa3063031933dd67017306
Jason Cheatham <jason@jasoncheatham.com>
2023-03-19 20:38:17 -0400

    fix: fix article marking

--- src/routes/api/articles/index.ts
+++ src/routes/api/articles/index.ts
@@ -4,6 +4,7 @@ export async function put(
 	data: ArticleUpdateRequest
 ): Promise<ArticleUpdateResponse> {
 	const resp = await fetch('/api/articles', {
+		method: 'PUT',
 		body: JSON.stringify(data)
 	});
 	const rdata = (await resp.json()) as ArticleUpdateResponse;
