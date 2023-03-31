98e69fad3ce759cd4992dd3538372b7984d9b260
Jason Cheatham <jason@jasoncheatham.com>
2023-03-31 18:21:07 -0400

    refactor: add types for PageData

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,27 @@
 import '@total-typescript/ts-reset';
 import type { SessionWithUser } from '$lib/db/session';
+import type { ArticleWithUserData } from '$lib/db/article';
+import type { FeedGroupWithFeeds } from '$lib/db/feedgroup';
+import type { Feed, FeedStats } from '$lib/db/feed';
 
 declare global {
-	declare namespace App {
+	namespace App {
 		interface Locals {
 			session?: SessionWithUser;
+		}
+
+		interface PageData {
+			articleFilter?: 'unread' | 'all';
+			articleId?: Article['id'];
+			articles?: ArticleWithUserData[];
+			feedGroups?: FeedGroupWithFeeds[];
+			feedId?: string;
+			feedName?: string;
+			feedStats?: FeedStats;
+			feeds?: Feed[];
+			selectedFeedIds?: Feed['id'][];
+			user: User;
+			userFeeds?: Feed[];
 		}
 	}
 }
