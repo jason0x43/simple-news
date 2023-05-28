0713e76f0af08f09cb565182810f3c43a5397916
Jason Cheatham <jason@jasoncheatham.com>
2023-05-28 14:02:14 -0400

    feat: protect API routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
 import { building } from '$app/environment';
 import { getSession } from '$lib/session';
-import type { Handle } from '@sveltejs/kit';
+import { text, type Handle } from '@sveltejs/kit';
 import { migrateToLatest } from './lib/db/lib/migrate';
 import { startDownloader } from './lib/feed.server';
 import { setLevel } from './lib/log';
@@ -14,8 +14,15 @@ if (!building) {
 }
 
 export const handle: Handle = async function ({ event, resolve }) {
-	const { cookies } = event;
-	event.locals.session = await getSession(cookies);
+	const session = await getSession(event.cookies);
 
+	// protect API paths
+	if (event.url.pathname.startsWith('/api') && !session) {
+		return text('Not authorized', {
+			status: 401
+		});
+	}
+
+	event.locals.session = session;
 	return resolve(event);
 };
