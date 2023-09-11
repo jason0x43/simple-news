4f196ae73a5b37b41182c40186fbe466c2c0a3ee
Jason Cheatham <jason@jasoncheatham.com>
2023-09-10 21:14:40 -0600

    fix: fix article close button

--- src/routes/reader/[feedId]/[articleId]/+page.svelte
+++ src/routes/reader/[feedId]/[articleId]/+page.svelte
@@ -80,7 +80,7 @@
 				{article}
 				{feed}
 				bind:this={articleView}
-				selectedFeedId={feed?.id}
+				selectedFeedId={data.feedId}
 			/>
 		{/if}
 	</div>
@@ -93,7 +93,7 @@
 				{article}
 				{feed}
 				bind:this={articleView}
-				selectedFeedId={feed?.id}
+				selectedFeedId={data.feedId}
 			/>
 		{/if}
 	</div>
