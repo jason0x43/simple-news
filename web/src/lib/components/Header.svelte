81c6bdbd0ec32332bd7c7c27c2c60866786daecb
Jason Cheatham <jason@jasoncheatham.com>
2024-11-03 10:46:56 -0700

    Add feed manager button to header

--- web/src/lib/components/Header.svelte
+++ web/src/lib/components/Header.svelte
@@ -2,7 +2,9 @@
 	import Button from "./Button.svelte";
 	import RssIcon from "../icons/Rss.svelte";
 	import UserIcon from "../icons/User.svelte";
+	import BackIcon from "../icons/Back.svelte";
 	import { goto } from "$app/navigation";
+	import { getAppContext } from "$lib/context";
 
 	let {
 		articleId,
@@ -13,6 +15,8 @@
 		feedId: string | undefined;
 		class?: string;
 	} = $props();
+
+	const managingFeeds = getAppContext("managingFeeds");
 
 	async function handleLogout() {
 		goto("/logout");
@@ -31,20 +35,35 @@
 		{className}
 	"
 >
-	<Button
-		class="p-2"
-		variant="invisible"
-		onclick={() => {
-			if (articleId) {
-				goto(`/feed/${feedId}`);
-			} else if (feedId) {
-				goto(`/`);
-			}
-		}}
-	>
-		<RssIcon size={22} />
-	</Button>
-	<Button class="p-2" variant="invisible" onclick={handleLogout}>
-		<UserIcon size={20} />
-	</Button>
+	<div>
+		{#if articleId || feedId}
+			<Button
+				class="p-2"
+				variant="invisible"
+				onclick={() => {
+					if (articleId) {
+						goto(`/feed/${feedId}`);
+					} else if (feedId) {
+						goto(`/`);
+					}
+				}}
+			>
+				<BackIcon size={22} />
+			</Button>
+		{/if}
+	</div>
+	<div class="flex flex-row sm:flex-col">
+		<Button
+			class="p-2"
+			variant="invisible"
+			onclick={() => {
+				$managingFeeds = true;
+			}}
+		>
+			<RssIcon size={20} />
+		</Button>
+		<Button class="p-2" variant="invisible" onclick={handleLogout}>
+			<UserIcon size={20} />
+		</Button>
+	</div>
 </header>
---
+++ web/src/lib/icons/Back.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let {
+		size = 30,
+	}: {
+		size?: number | string;
+	} = $props();
+</script>
+
+<svg height={size} width={size} viewBox="0 -960 960 960" fill="currentColor">
+	<path d="M640-80 240-480l400-400 71 71-329 329 329 329-71 71Z" />
+</svg>
